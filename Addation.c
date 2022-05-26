de2bf48211900bd3d7374933713d44110a0cb28b
Nazmul hassan samrat <105812543+Nazmul4952@users.noreply.github.com>
2022-05-26 23:17:45 +0800

    c addation /operator

---
+++ Addation.c
@@ -0,0 +1,25 @@
+/*#include<stdio.h>
+int main()
+{
+    int x=12;
+    int result=x+8;
+    printf("%d",result);
+
+
+
+
+    return 0;
+}*/
+#include<stdio.h>
+int main()
+{
+    int =x ,y,sum;
+    printf(" Enter the value :");
+    scanf( " %d%d",&sum);
+    sum=x+y;
+    printf("%d",sum);
+
+
+
+    return 0;
+}
