223e9578f36f98576f32fa1de38713cb10d743a9
Nazmul hassan samrat <105812543+Nazmul4952@users.noreply.github.com>
2022-06-02 01:58:12 +0600

    division\operator

---
+++ Division.c
@@ -0,0 +1,50 @@
+/*#include<stdio.h>
+int main ()
+{
+  int x=12;
+  int result=x/2;
+  printf(" %d",result);
+
+
+
+    return 0;
+}*/
+/*#include<stdio.h>
+int main()
+{
+    int x=5;
+    int result =x*4;
+    printf("%d",result);
+
+
+
+    return 0;
+
+}*/
+
+
+#include<stdio.h>
+int main()
+{
+    int x=20;
+    int result =x-8;
+    printf("%d",result);
+
+
+
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
