e15c394ea1ce02133a7fea96d3b68dffdd4e0504
Nazmul hassan samrat <105812543+Nazmul4952@users.noreply.github.com>
2022-06-02 02:13:05 +0600

    for break loop

---
+++ exersice 4.c
@@ -0,0 +1,44 @@
+/*#include<stdio.h>
+int main()
+{
+	int i;
+	
+	for(i=1;i<11;i++){
+		
+		if(i==9){
+			
+		 break;	
+			
+		}
+		
+		
+	   printf("%d\n",i);
+		
+	}
+	
+	printf("Out of loop");
+	
+	return 0;
+}*/
+#include<stdio.h>
+int main()
+{
+int i;
+for (i=1; i<11;i++){
+	
+	if(i==3){
+		break;
+	}
+}	
+	printf("%d\n",i);
+}
+printf("out of loop");
+
+	
+	
+	
+	
+	return 0;
+	
+	
+}
