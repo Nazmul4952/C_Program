b4f1873c29cd657a8d41008ad1b4d0ca883296fe
Nazmul hassan samrat <105812543+Nazmul4952@users.noreply.github.com>
2022-06-02 01:59:22 +0600

    increment&decrement operator

---
+++ increment &decrement operator.c
@@ -0,0 +1,21 @@
+/*#include<stdio.h>
+int main()
+{
+  int x=12;
+  printf(" %d",++x);//increment
+
+
+    return 0;
+}*/
+
+#include<stdio.h>
+int main()
+{
+    int x=21;
+    printf("%d",--x);
+
+
+
+
+    return 0;
+}
