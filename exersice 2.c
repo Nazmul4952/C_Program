4b068106356e1db2e2276e7b31c77f660d0a3845
Nazmul hassan samrat <105812543+Nazmul4952@users.noreply.github.com>
2022-06-02 02:12:37 +0600

    for loop

---
+++ exersice 2.c
@@ -0,0 +1,28 @@
+/*#include<stdio.h>
+int main()
+{
+	int i;
+	for(i=1; i<=10;i++)
+	{
+		
+		
+		printf("%d\n",i);
+	}
+	
+	
+	return 0;
+}*/
+
+#include<stdio.h>
+int main ()
+{
+int i;
+for(i=100;i>1;i--)
+{
+	printf("Enter the number:");
+printf("%d\n",i);	
+}	
+	
+	
+	return 0;
+}
