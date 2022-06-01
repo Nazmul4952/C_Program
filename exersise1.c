09201585102141904e4384dce87a1eb8aa52b740
Nazmul hassan samrat <105812543+Nazmul4952@users.noreply.github.com>
2022-06-02 02:12:06 +0600

    while loop

---
+++ exersise1.c
@@ -0,0 +1,34 @@
+/*#include<stdio.h>
+int main()
+{
+	int i=1;
+	while(i<11){
+		
+		printf("enter the number:");
+		printf("%d\n",i);
+		i++;
+	}
+	
+	
+	
+	
+	
+	
+	return 0;
+}*/
+
+#include<stdio.h>
+int main()
+{
+	
+	int i=10;
+	while(i>0){
+	printf("%d\n",i);
+	i--;	
+		
+		
+	}
+	
+	
+	return 0;
+}
